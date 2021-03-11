c39af4cd73c3cdd550fa2e2935b3e7d59ced33de
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-11 22:19:54 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
 firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in 
