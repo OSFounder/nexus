a6991d627f3e593654217cb8d2034eea0da75b18
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-13 10:34:15 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAkuL0kZgwpJZpv0eJF8N_fh3mpsYCrFNQ",
-    authDomain: "nexus-database-b7220.firebaseapp.com",
-    projectId: "nexus-database-b7220",
-    storageBucket: "nexus-database-b7220.appspot.com",
-    messagingSenderId: "75486796978",
-    appId: "1:75486796978:web:d4fb7013ad6131a6257dab",
-    measurementId: "G-RQNWQVCW9T"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+(function() {
 
-//get data
-const email = document.getElementById('email_field');
-const pword = document.getElementById('pass_field');
-const signin = document.getElementById('log-in');
-const signup = document.getElementById('sign-up')
-const signout = document.getElementById('log-out');
+    var firebaseConfig = {
+        apiKey: "AIzaSyAkuL0kZgwpJZpv0eJF8N_fh3mpsYCrFNQ",
+        authDomain: "nexus-database-b7220.firebaseapp.com",
+        projectId: "nexus-database-b7220",
+        storageBucket: "nexus-database-b7220.appspot.com",
+        messagingSenderId: "75486796978",
+        appId: "1:75486796978:web:d4fb7013ad6131a6257dab",
+        measurementId: "G-RQNWQVCW9T"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-  } else {
-    // No user is signed in.
-  }
+      //get data
+      const txtemail = document.getElementById('email_field');
+      const txtpword = document.getElementById('pass_field');
+      const binsignin = document.getElementById('log-in');
+      const binsignup = document.getElementById('sign-up')
+      const binsignout = document.getElementById('log-out');
  
-});
-function begin() {
- const userEmail = document.getElementById("field_email").value;
- const userPassword = document.getElementById("field_password").value;
- 
- firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    alert("Logged in")
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert("Email and or password wrong, try again.")
-  });
-}
+      //listen for sign in attempt
+      signin.addEventListner('click', e => {
+          //get email and pword
+          const email = txtemail.value();
+          const pass = txtpword.value();
+          const auth = firebase.auth();
+          //sign in
+          const promise = auth.signInWithEmailAndPassword(email, pass);
+          
+          
+      });
+
+}());
