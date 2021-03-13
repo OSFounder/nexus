1afa2ab7dc9870af393a31c395607f88ccb0b516
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-14 09:44:32 +1300

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,20 @@ function authWithCredential(credential) {
     .then((result) => {
       // Signed in 
       // ...
-      setPersistenceSession()
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+       .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
     })
     .catch((error) => {
       // Handle Errors here.
