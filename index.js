c168066b5a4c8cac56ad7b2532c9ea7674480f70
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-14 08:23:02 +1300

    Update index.js

--- index.js
+++ index.js
@@ -50,10 +50,12 @@ function authStateListener() {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       var uid = user.uid;
+      console.log(uid)
       // ...
     } else {
       // User is signed out
       // ...
+      console.log('logged out')
     }
   });
   // [END auth_state_listener]
