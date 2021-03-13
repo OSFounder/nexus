0149fe3445a7bbcb96afe1e39bd1289ecc6916de
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-13 13:48:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/ These samples are intended for Web so this import would normally be
-// done in HTML however using modules here is more convenient for
+
 // ensuring sample correctness offline
 import firebase from "firebase/app";
 import "firebase/auth";
