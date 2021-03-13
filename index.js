05faf67c63c7652c3ba7fc934ccdbfb459581f89
OSFounder <65999136+OSFounder@users.noreply.github.com>
2021-03-13 14:36:59 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,26 @@
 (function() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyAkuL0kZgwpJZpv0eJF8N_fh3mpsYCrFNQ",
-        authDomain: "nexus-database-b7220.firebaseapp.com",
-        projectId: "nexus-database-b7220",
-        storageBucket: "nexus-database-b7220.appspot.com",
-        messagingSenderId: "75486796978",
-        appId: "1:75486796978:web:d4fb7013ad6131a6257dab",
-        measurementId: "G-RQNWQVCW9T"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+   //initialize application
+   const config = {
+      apiKey: "AIzaSyAkuL0kZgwpJZpv0eJF8N_fh3mpsYCrFNQ",
+      authDomain: "nexus-database-b7220.firebaseapp.com",
+      databaseURL: "https://nexus-database-b7220-default-rtdb.firebaseio.com",
+      projectId: "nexus-database-b7220",
+      storageBucket: "nexus-database-b7220.appspot.com",
+      messagingSenderId: "75486796978",
+      appId: "1:75486796978:web:d4fb7013ad6131a6257dab",
+      measurementId: "G-RQNWQVCW9T"
+};
+  firebase.initializeApp(config);
     
-      var user = firebase.auth().currentUser;
-      if (user) {
-        // User is signed in.
-      } else {
-        // No user is signed in.
-      }
-      //get data
-      const txtemail = document.getElementById('email_field');
-      const txtpword = document.getElementById('pass_field');
-      const binsignin = document.getElementById('log-in');
-      const binsignup = document.getElementById('sign-up')
-      const binsignout = document.getElementById('log-out');
- 
-      //listen for sign in attempt
-      binsignin.addEventListener('click', e => {
-          //get email and pword
-          const email = txtemail.value;
-          const pass = txtpword.value;
-          const auth = firebase.auth();
-          //sign in
-          const promise = auth.signInWithEmailAndPassword(email, pass);
-          promise.catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
-          
-      });
-        binsignup.addEventListener('click', e => {
-          //get email and pword
-          const email = txtemail.value;
-          const pass = txtpword.value;
-          const auth = firebase.auth();
-          //sign in
-          const promise = auth.createUserWithEmailAndPassword(email, pass);
-          promise.catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ..
-             console.log(errorCode);
-              console.log(errorMessage);
-          
-      });
+   const txtEmail = document.getElementById('email_field');
+   const txtPword = document.getElementById('pass_field');
+   const binSignIn = document.getElementById('sign-in');
+   const binSignUp = document.getElementById('sign-up');
+   const binSignOut = document.getElementById('sign-out');
+   
+   //add login event
+   binSignIn.addEventListener('click', e => {
        
-      firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            // User is signed in.
-              console.log(user);
-          } else {
-            // No user is signed in.
-              console.log('not logged in');
-          }
-});
+   });
+    
 }());
